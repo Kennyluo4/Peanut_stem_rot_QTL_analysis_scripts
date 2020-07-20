bb3e28e68e69fa30167f2ad840961044bae8c778
Ziliang Luo <kennyluo4@gmail.com>
2020-07-19 22:06:32 -0400

    Update searchGeneInQTL.py

--- searchGeneInQTL.py
+++ searchGeneInQTL.py
@@ -1,8 +1,10 @@
+#The QTL file (e.g. "consensusQTLlist.txt") should have 1st column as QTL name, 3rd column as chromosome, 5th column as start, 6th column as end.
+#The gene file (e.g. "WMmap.txt") should have 3rd as chromosome, 4th column as gene start, 5th as gene end
 result = []
 
 for lines in open('consensusQTLlist.txt'):
-      items = lines.split('\t')
-      qtl, chr, left, right = items[0],items[2], items[4], items[5]
+      cols = lines.split('\t')
+      qtl, chr, left, right = cols[0],cols[2], cols[4], cols[5]
       left = float(left)
       right = float(right)
       qtl_info = lines.replace("\n", "\t")
